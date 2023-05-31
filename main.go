4d8b00ac86448ba93be11834ef88e11a6b2c5ff7
null <hikarudjent@gmail.com>
2023-05-31 22:12:57 +0900

    取得、更新、削除

--- main.go
+++ main.go
@@ -18,4 +18,14 @@ func main() {
 
 	u, _ := models.GetUser(1)
 	fmt.Println(u)
+
+	u.Name = "test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 }
