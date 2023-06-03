26d5303580010ad121ed0eb2e3bde0680ee1323e
null <hikarudjent@gmail.com>
2023-06-03 23:31:28 +0900

    todoの取得、更新、削除

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main() {
 	fmt.Println(models.Db)
 
 	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.PassWord = "testtest"
+	// u.Name = "test3"
+	// u.Email = "test3@example.com"
+	// u.PassWord = "testtesttest"
 	// fmt.Println(u)
 
 	// u.CreateUser()
@@ -32,6 +32,20 @@ func main() {
 	// user, _ := models.GetUser(2)
 	// user.CreateTodo("first todo")
 
-	t, _ := models.GetTodo(1)
-	fmt.Println(t)
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("third todo")
+
+	// user2, _ := models.GetUser(2)
+	// todos, _ := user2.GetTodosByUser()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	t, _ := models.GetTodo(3)
+	// t.Content = "update todo"
+	// t.UpdateTodo()
+	t.DeleteTodo()
+
 }
