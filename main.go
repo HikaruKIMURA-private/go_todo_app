97649e5dee31e4374270d8e52fe05512978ede9f
null <hikarudjent@gmail.com>
2023-06-04 22:34:00 +0900

    top画面実装、jquery, bootstrap

--- main.go
+++ main.go
@@ -2,50 +2,12 @@ package main
 
 import (
 	"fmt"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test3"
-	// u.Email = "test3@example.com"
-	// u.PassWord = "testtesttest"
-	// fmt.Println(u)
-
-	// u.CreateUser()
-
-	// u, _ := models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.Name = "test2"
-	// u.Email = "test2@example.com"
-	// u.UpdateUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// user, _ := models.GetUser(2)
-	// user.CreateTodo("first todo")
-
-	// t, _ := models.GetTodo(1)
-	// fmt.Println(t)
-	// user, _ := models.GetUser(3)
-	// user.CreateTodo("third todo")
-
-	// user2, _ := models.GetUser(2)
-	// todos, _ := user2.GetTodosByUser()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	t, _ := models.GetTodo(3)
-	// t.Content = "update todo"
-	// t.UpdateTodo()
-	t.DeleteTodo()
-
+	controllers.StartMainServer()
 }
