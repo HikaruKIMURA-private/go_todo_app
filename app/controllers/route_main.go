2f0feb80c2827795d994b4482d642ef50beda0d5
null <hikarudjent@gmail.com>
2023-06-06 22:22:45 +0900

    登録ページ追加

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,9 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
-	"text/template"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		log.Println(err)
-	}
-	t.Execute(w, "hello world")
+	generateHTML(w, "Hello", "layout", "public_navbar", "top")
 }
