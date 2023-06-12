750ee9a3a0d39f2e742f11cb434487d370b53756
null <hikarudjent@gmail.com>
2023-06-12 22:08:49 +0900

    ログアウト実装

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"todo_app/app/models"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
@@ -14,10 +17,31 @@ func top(w http.ResponseWriter, r *http.Request) {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	_, err := session(w, r)
+	sess, err := session(w, r)
 	if err != nil {
 		http.Redirect(w, r, "/", 302)
 	} else {
-		generateHTML(w, nil, "layout", "private_navbar", "index")
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		todos, _ := user.GetTodosByUser()
+		user.Todos = todos
+		fmt.Println("aaaaa")
+		fmt.Println(user)
+		generateHTML(w, user, "layout", "private_navbar", "index")
 	}
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("_cookie")
+	if err != nil {
+		log.Println(err)
+	}
+
+	if err != http.ErrNoCookie {
+		session := models.Session{UUID: cookie.Value}
+		session.DeleteSessionByUUID()
+	}
+	http.Redirect(w, r, "/login", 302)
 }
